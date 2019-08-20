9b6d482af5baeb8566d6b7153577903c21358e69
Lisa einstein <lisaeinstein@DN0a23313f.SUNet>
2019-08-20 15:04:01 -0700

    up to FLigne

--- FrGraphics/fr/graphics/FLigne.java
+++ FrGraphics/fr/graphics/FLigne.java
@@ -3,48 +3,48 @@ import java.awt.Color;
 
 import acm.graphics.GLine;
 
-public class SLinea extends GLine implements SObjeto{
+public class FLigne extends GLine implements FObjet{
 
-	public SLinea(double x0, double y0, double x1, double y1) {
+	public FLigne(double x0, double y0, double x1, double y1) {
 		super(x0, y0, x1, y1);
 	}
 
 	/* Getters and setters for start and end points. */
-	public void cambiarPuntoPrincipio(double x, double y) {
+	public void placerPointDeDepart(double x, double y) {
 		setStartPoint(x, y);
 	}
-	public void cambiarPuntoFinal(double x, double y) {
+	public void placerPointFinal(double x, double y) {
 		setEndPoint(x, y);
 	}
-	public double darPuntoPrincipioX() {
+	public double lirePointDeDepartX() {
 		return getStartPoint().getX();
 	}
-	public double darPuntoPrincipioY() {
+	public double lirePointDeDepartY() {
 		return getStartPoint().getY();
 	}
-	public double darPuntoFinalX() {
+	public double lirePointFinalX() {
 		return getEndPoint().getX();
 	}
-	public double darPuntoFinalY() {
+	public double lirePointFinalY() {
 		return getEndPoint().getY();
 	}
 
-	public double darX() {
-		return darPuntoPrincipioX();
+	public double lireX() {
+		return lirePointDeDepartX();
 	}
-	public double darY() {
-		return darPuntoPrincipioY();
+	public double lireY() {
+		return lirePointDeDepartY();
 	}
 
-	public void moverse(double dx, double dy) {
+	public void deplacer(double dx, double dy) {
 		move(dx, dy);
 	}
 
-	public void cambiarUbicacion(double i, double j) {
+	public void emplacer(double i, double j) {
 		setLocation(i, j);
 	}
 
-	public void cambiarColor(Color c) {
+	public void appliquerCouleur(Color c) {
 		setColor(c);
 	}
 	
